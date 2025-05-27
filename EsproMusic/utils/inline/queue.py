542d2d43e19e324dfda2bbf2f76c4298555cd647
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-27 20:26:05 +0530

    Update queue.py

--- EsproMusic/utils/inline/queue.py
+++ EsproMusic/utils/inline/queue.py
@@ -71,5 +71,7 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+          [InlineKeyboardButton(text=" ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ", url=f"https://t.me/MAHI_X_MUSIC_BOT?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users")],   
+        
     ]
     return buttons
