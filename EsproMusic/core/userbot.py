f49aa9a4afe1b9c238c644c80409b9284d943a62
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-27 20:10:25 +0530

    Update userbot.py

--- EsproMusic/core/userbot.py
+++ EsproMusic/core/userbot.py
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("EsproSupport")
-                await self.two.join_chat("EsproUpdate")
+                await self.two.join_chat("All_super_music")
+                await self.two.join_chat("Sweet_chat_aao")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("EsproSupport")
-                await self.three.join_chat("EsproUpdate")
+                await self.three.join_chat("All_super_music")
+                await self.three.join_chat("Sweet_chat_aao")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("EsproSupport")
-                await self.four.join_chat("EsproUpdate")
+                await self.four.join_chat("All_super_music")
+                await self.four.join_chat("Sweet_chat_aao")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("EsproSupport")
-                await self.five.join_chat("EsproUpdate")
+                await self.five.join_chat("All_super_music")
+                await self.five.join_chat("Sweet_chat_aao")
             except:
                 pass
             assistants.append(5)
