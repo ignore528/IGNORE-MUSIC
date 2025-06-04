0e85282b3dc33a02855b190156e186fe322d85e8
ignore528 <aliakram7548@gmail.com>
2025-06-04 06:44:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/IGNOREMUSICPRO/IGNORE-MUSIC",
+    "https://github.com/ignore528/IGNORE-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
